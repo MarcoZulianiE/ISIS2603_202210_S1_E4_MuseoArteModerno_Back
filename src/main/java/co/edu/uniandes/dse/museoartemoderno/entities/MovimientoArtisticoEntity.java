0ab5afdf25e2a9959a9728f7fe0cde3d0056efd2
Juan Sebastian Pardo <j.pardor@uniandes.edu.co>
2022-02-22 08:22:13 -0500

    Correccion notacion Podam

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/MovimientoArtisticoEntity.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/MovimientoArtisticoEntity.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.Setter;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Entity
 @Getter
@@ -19,26 +22,26 @@ public class MovimientoArtisticoEntity extends BaseEntity{
 	private String nombre;
 
 
-	@PodamExclusive
+	@PodamExclude
 	@OneToMany
 	List<ObraEntity> obras = new ArrayList<>();
 
 
-	@PodamExclusive
+	@PodamExclude
 	@ManyToMany
 	List<ArtistaEntity> artistas = new ArrayList<>();
 
-	@PodamExclusive
+	@PodamExclude
 	@ManyToMany(
 		mappedBy = "museos")
 	List<MuseoEntity> museos = new ArrayList<>();
 
-	@PodamExclusive
+	@PodamExclude
 	@ManyToOne(
 		mappedBy = "lugarOrigen")
 	PaisEntity lugarOrigen;
 
-	@PodamExclusive
+	@PodamExclude
 	@ManyToMany
 	List<FechaEntity> fechasApogeo = new ArrayList<>();
 }

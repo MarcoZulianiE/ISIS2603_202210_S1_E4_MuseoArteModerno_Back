9b800f45a74a57a5fe4f257c1f0bce3774c3218f
Nicolas <n.maldonado2@uniandes.edu.co>
2022-02-24 09:17:54 -0500

    repositories Pais y Ciudad

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/PaisEntity.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/PaisEntity.java
@@ -3,13 +3,18 @@ package co.edu.uniandes.dse.museoartemoderno.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import co.edu.uniandes.dse.bookstore.entities.BookEntity;
+import lombok.Getter;
+import lombok.Setter;
 import uk.co.jemos.podam.common.PodamExclude;
 
-public class PaisEntity {
+@Entity
+@Getter
+@Setter
+public class PaisEntity extends BaseEntity{
 	private String nombrePais;
 	
 	@PodamExclude
---
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/repositories/PaisRepository.java
@@ -0,0 +1,9 @@
+package co.edu.uniandes.dse.museoartemoderno.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.uniandes.dse.museoartemoderno.entities.PaisEntity;
+
+public interface PaisRepository extends JpaRepository<PaisEntity, Long> {
+
+}

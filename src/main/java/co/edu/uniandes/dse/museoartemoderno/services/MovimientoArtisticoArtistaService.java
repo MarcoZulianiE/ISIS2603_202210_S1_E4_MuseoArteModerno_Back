6566978af38bdc185d24f5a772c6d2d50991cf10
Juan Sebastian Pardo <j.pardor@uniandes.edu.co>
2022-02-25 09:24:50 -0500

    Creacion metodo de getArtista en servicio de movimiento-artista

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/services/MovimientoArtisticoArtistaService.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/services/MovimientoArtisticoArtistaService.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import co.edu.uniandes.dse.museoartemoderno.entities.ArtistaEntity;
 import co.edu.uniandes.dse.museoartemoderno.entities.MovimientoArtisticoEntity;
 import co.edu.uniandes.dse.museoartemoderno.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.museoartemoderno.exceptions.IllegalOperationException;
 import co.edu.uniandes.dse.museoartemoderno.repositories.ArtistaRepository;
 import co.edu.uniandes.dse.museoartemoderno.repositories.MovimientoArtisticoRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -83,5 +84,35 @@ public class MovimientoArtisticoArtistaService
 		}
 		log.info("Finaliza proceso de obtener todos los artistas asociados con el movimiento artistico "+movimientoId);
 		return artistaList;
+	}
+	
+	/**
+	 * Obtiene una instancia de ArtistaEntity asociada a una instancia de MovimientoArtisticoEntity
+	 * @param movimientoId - MovimientoArtista
+	 * @param artistaId - Artista
+	 * @throws EntityNotFoundException si el artista o el movimiento no existe
+	 * @return La entidad de Artista del movimiento
+	 */
+	public ArtistaEntity getArtista(Long movimientoId, Long artistaId) throws EntityNotFoundException, IllegalOperationException
+	{
+		log.info("Inicia el proceso de obtener el artista "+artistaId+" asociado con el movimiento "+movimientoId);
+		Optional<MovimientoArtisticoEntity> movimientoEntity = movimientoArtisticoRepository.findById(movimientoId);
+		Optional<ArtistaEntity> artistaEntity = artistaRepository.findById(artistaId);
+
+		if(movimientoEntity.isEmpty())
+		{
+			throw new EntityNotFoundException("Movimiento artistico not found");
+		}
+		if(artistaEntity.isEmpty())
+		{
+			throw new EntityNotFoundException("Artista not found");
+		}
+		log.info("Termina el proceso de obtener el artista "+artistaId+" asociado con el movimiento "+movimientoId);
+		if(artistaEntity.get().getMovimientos().contains(movimientoEntity.get()))
+		{
+			return artistaEntity.get();
+		}
+
+		throw new IllegalOperationException("El artista no esta asociado con el movimiento artistico");
 	}
 }

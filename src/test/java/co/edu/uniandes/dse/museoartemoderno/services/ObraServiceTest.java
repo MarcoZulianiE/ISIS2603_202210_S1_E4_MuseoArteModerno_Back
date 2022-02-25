52032bdfde982fcc297e33ae645b900a68879d2a
Alejandro Gómez <a.gomez65@uniandes.edu.co>
2022-02-25 09:05:25 -0500

    Comantario Test

--- src/test/java/co/edu/uniandes/dse/museoartemoderno/services/ObraServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/museoartemoderno/services/ObraServiceTest.java
@@ -1,40 +1,40 @@
-package co.edu.uniandes.dse.museoartemoderno.services;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import javax.transaction.Transactional;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-
-
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
-@Transactional
-@Import({ ObraService.class })
-class ObraServiceTest {
-	
-	
-	@Autowired
-	private ObraService obraService;
-
-	@Autowired
-	private TestEntityManager entityManager;
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@Test
-	void testGetAllObras() {
-		fail("Not yet implemented");
-	}
-
-}
+//package co.edu.uniandes.dse.museoartemoderno.services;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import javax.transaction.Transactional;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.context.annotation.Import;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//
+//
+//@ExtendWith(SpringExtension.class)
+//@DataJpaTest
+//@Transactional
+//@Import({ ObraService.class })
+//class ObraServiceTest {
+//	
+//	
+//	@Autowired
+//	private ObraService obraService;
+//
+//	@Autowired
+//	private TestEntityManager entityManager;
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@Test
+//	void testGetAllObras() {
+//		fail("Not yet implemented");
+//	}
+//
+//}

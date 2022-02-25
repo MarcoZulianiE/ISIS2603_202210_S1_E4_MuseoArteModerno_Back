ffae1244d137021aa3d2b5803d65c09313c75cbb
Alejandro Gómez <a.gomez65@uniandes.edu.co>
2022-02-24 21:25:50 -0500

    Entity, Repository y ObraMuseoService implementados

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/ObraEntity.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/entities/ObraEntity.java
@@ -1,26 +1,37 @@
 package co.edu.uniandes.dse.museoartemoderno.entities;
 
+import uk.co.jemos.podam.common.PodamExclude;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import co.edu.uniandes.dse.museoartemoderno.entities.ObraEntity;
+import lombok.Getter;
+import lombok.Setter;
+
 
 @Getter
 @Setter
 @Entity
-
 public class ObraEntity extends BaseEntity{
 	
 	private String nombre;
 	private String tipo;
 	private String descripcion;
 	
-	
+	@PodamExclude
 	@ManyToOne
-	private MuseoEntity museoDeExhibicion;
+	private MuseoEntity museoExhibicion;
 	
+	@PodamExclude
 	@ManyToOne
 	private ArtistaEntity autor;
 	
+	@PodamExclude
 	@ManyToOne
 	private FechaEntity fechaPublicacion;
 	
+	@PodamExclude
 	@ManyToOne
 	private MovimientoArtisticoEntity movimiento;
 	

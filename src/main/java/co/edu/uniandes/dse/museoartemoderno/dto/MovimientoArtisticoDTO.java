a99107de3e945ca27dd930392c98f7b0ce659853
Juan Sebastian Pardo <j.pardor@uniandes.edu.co>
2022-03-01 13:34:48 -0500

    Creacion del DTO para MovimientoArtistico

---
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/dto/MovimientoArtisticoDTO.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.museoartemoderno.dto;
+
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovimientoArtisticoDTO 
+{
+	private String nombre;
+	private Date fechaApogeo;
+}

3235cbbf1acce73a2688e5d7a0b3598459e5a744
Daniel Bernal <78066378+HIPERMAST@users.noreply.github.com>
2022-03-02 01:14:43 -0500

    Fixing errors

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/repositories/MuseoRepository.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/repositories/MuseoRepository.java
@@ -1,5 +1,7 @@
 package co.edu.uniandes.dse.museoartemoderno.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import co.edu.uniandes.dse.museoartemoderno.entities.MuseoEntity;
 
 @Repository
 public interface MuseoRepository extends JpaRepository<MuseoEntity, Long> {
-	
+	List<MuseoEntity> findByNombre(String nombre);
 }
--- src/main/java/co/edu/uniandes/dse/museoartemoderno/services/MuseoService.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/services/MuseoService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import co.edu.uniandes.dse.museoartemoderno.repositories.MuseoRepository;
 import co.edu.uniandes.dse.museoartemoderno.entities.MuseoEntity;
 import co.edu.uniandes.dse.museoartemoderno.exceptions.EntityNotFoundException;
+import co.edu.uniandes.dse.museoartemoderno.exceptions.ErrorMessage;
 import co.edu.uniandes.dse.museoartemoderno.exceptions.IllegalOperationException;
 
 import lombok.extern.slf4j.Slf4j;
@@ -31,14 +32,31 @@ public class MuseoService {
 	 */
 	@Transactional
 	public MuseoEntity createMuseo(MuseoEntity museoEntity) throws EntityNotFoundException, IllegalOperationException {
-		log.info("Inicia el proceso de creación del museo");
+		log.info("Inicia proceso de creación del Museo");
 		
-	
-		log.info("Termina el proceso de creación del museo");
+		if (!validateNombre(museoEntity.getNombre()))
+			throw new IllegalOperationException("Nombre is not valid");	
+		
+		if (!museoRepository.findByNombre(museoEntity.getNombre()).isEmpty())
+			throw new IllegalOperationException("Nombre already exists");
+
+		if (museoEntity.getUbicacion() == null)
+			throw new IllegalOperationException("Ubicacion is not valid");	
+		
+		if (museoEntity.getMovimientos() == null)
+			throw new IllegalOperationException("Movimientos is not valid");
+		
+		if (museoEntity.getArtistas() == null)
+			throw new IllegalOperationException("Artistas is not valid");
+		
+		if (museoEntity.getObras() == null)
+			throw new IllegalOperationException("Obras is not valid");
+		
+		log.info("Termina proceso de creación del Museo");
 		return museoRepository.save(museoEntity);
 	}
-    
-    
+
+
 	/**
 	 * @return Lista de todas las entidades de tipo Museo
 	 */
@@ -60,13 +78,13 @@ public class MuseoService {
 		log.info("Inicia proceso de consultar el museo con id: " + museoId);
 		Optional<MuseoEntity> museoEntity = museoRepository.findById(museoId);
 		
-		//TODO
 		if (museoEntity.isEmpty())
 			throw new EntityNotFoundException("MUSEO NOT FOUND");
 		
 		log.info("Termina proceso de consultar el museo con id: " + museoId);
 		return museoEntity.get();
 	}
+	
 	
 	/**
 	 * Actualizar un museo dado su Id
@@ -82,10 +100,27 @@ public class MuseoService {
 		log.info("Inicia proceso de actualizar el museo con id: " + museoId);
 		Optional<MuseoEntity> museoEntity = museoRepository.findById(museoId);
 		
-		//TODO
 		if (museoEntity.isEmpty())
-			throw new EntityNotFoundException("MUSEO NOT FOUND");
+			throw new EntityNotFoundException(ErrorMessage.MUSEO_NOT_FOUND);
+		
+		if (!validateNombre(museo.getNombre()))
+			throw new IllegalOperationException("Nombre is not valid");	
+		
+		if (!museoRepository.findByNombre(museo.getNombre()).isEmpty())
+			throw new IllegalOperationException("Nombre already exists");
 
+		if (museo.getUbicacion() == null)
+			throw new IllegalOperationException("Ubicacion is not valid");	
+		
+		if (museo.getMovimientos() == null)
+			throw new IllegalOperationException("Movimientos is not valid");
+		
+		if (museo.getArtistas() == null)
+			throw new IllegalOperationException("Artistas is not valid");
+		
+		if (museo.getObras() == null)
+			throw new IllegalOperationException("Obras is not valid");
+		
 		museo.setId(museoId);
 		log.info("Termina proceso de actualizar el museo con id: " + museoId);
 		return museoRepository.save(museo);
@@ -102,13 +137,22 @@ public class MuseoService {
 		log.info("Inicia proceso de borrar el museo con id: ", museoId);
 		Optional<MuseoEntity> museoEntity = museoRepository.findById(museoId);
 		
-		//TODO
 		if (museoEntity.isEmpty())
-			throw new EntityNotFoundException("MUSEO NOT FOUND");
+			throw new EntityNotFoundException(ErrorMessage.MUSEO_NOT_FOUND);
 		
 		museoRepository.deleteById(museoId);
 		log.info("Termina proceso de borrar el museo con id: " + museoId);
 	}
 	
+	
+	/**
+	 * Verifica que el Nombre sea valido.
+	 *
+	 * @param nombre que se debe verificar
+	 * @return true si el ISBN es valido.
+	 */
+	private boolean validateNombre(String nombre) {
+		return !(nombre == null || nombre.isEmpty());
+	}
 	
 }

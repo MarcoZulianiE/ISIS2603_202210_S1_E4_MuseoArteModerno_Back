edc30693e38f858bdc02801a651148375c7dec9b
SantiagoRC <s.rodriguez52@uniandes.edu.co>
2022-03-10 17:37:43 -0500

    avance controller ciudad

--- src/main/java/co/edu/uniandes/dse/museoartemoderno/controllers/CiudadController.java
+++ src/main/java/co/edu/uniandes/dse/museoartemoderno/controllers/CiudadController.java
@@ -14,8 +14,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.beans.factory.annotation.Autowired;
+import co.edu.uniandes.dse.museoartemoderno.services.CiudadService;
+@RestController
+@RequestMapping("/ciudades")
 public class CiudadController {
-
+    @Autowired
+	private CiudadService ciudadService;
+    @Autowired
+    private ModelMapper modelMapper;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+
